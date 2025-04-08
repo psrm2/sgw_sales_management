9357fda8a79f14ad52834a1dfa996d2e3c7c22a8
BuildTools <unconfigured@null.spigotmc.org>
2025-04-08 23:43:54 +0900

    V1.1.25

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,10 @@
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated() || req.session.admin) {
+    // 各リクエストごとにセッションの有効期限を 30 日に延長する
+    req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000; // 30日分のミリ秒
     return next();
   }
   res.redirect('/login');
 }
+
 module.exports = { ensureAuthenticated };
